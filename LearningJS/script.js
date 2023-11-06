aa8f46921a1318cd18540cfa3ceba7637ba210c7
Insecupa <abhijeetrayapeddi@gmail.com>
2023-11-07 00:07:24 +0530

    Understanding async and await

--- LearningJS/script.js
+++ LearningJS/script.js
@@ -1,124 +1,48 @@
-// console.log("Welcome to Temparature Convertor")
 
-// let temp = document.getElementById("tempValue").value
-// console.log(temp)
+// let promise = new Promise(
+//     (resolve, reject) => {
+//         let fileLoad = false
 
-// document.getElementById("submit").onclick = function() {
+//         if(fileLoad) {
+//             resolve("FIle is loaded and all")
+//         } else {
+//             reject("FIle is not loaded and all")
+//         }
+//     }
+// )
 
-//     temp = document.getElementById("tempValue").value
+// promise.then(value => console.log(value))
+//     .catch(value => console.error(value))
 
+// let wait = time => new Promise( resolve => {
+//     setTimeout(resolve, time)
+// })
+
+// wait(3000).then(() => console.log("Thanks for waiting"))
+
+//async funcito returns a promise
+
+async function loadFile() {
+
+    let fileLoad = true
+
+    if(fileLoad) 
+        return "File is loaded"
     
-//     if(!Boolean(temp)) {
-//         document.getElementById("answer").innerHTML = "Please enter a value"
-//     }
+    else throw "File not load"
 
-//     else if(document.getElementById("celsius").checked) {
-//         document.getElementById("answer").innerHTML = "Celsius value is " + calculate(temp, "celsius")
-//     }
-//     else if(document.getElementById("far").checked) {
-//         document.getElementById("answer").innerHTML = "Farenheit value is " + calculate(temp, "far")
-//     }
-//     else{
-//         console.log("Select a type to convert the temp to")
-//     }
-
-// }
-
-// function calculate(temparatureValue, typ) {
-
-//     temparatureValue = Number(temparatureValue)
-    
-//     if(String(typ).match("celsius")) {
-//        return (temparatureValue - 32) * (5/9)
-//     }
-
-//     else if(String(typ).match("far")) {
-//         return temparatureValue * 9 / 5 + 32
-//     }
-
-// }
-
-// diff(2,3, somethingDoes)
-
-// function diff(x, y, funcitonOfSomething) {
-//     funcitonOfSomething(x - y);
-// }
-
-// function somethingDoes(output) {
-//     console.log(output)
-// }
-
-// //Callback functions
-
-// //Array for each
-// let numbersAdd = [1,3,5,10]
-
-
-// function addFive(element, index, array) {
-//    return  element + 5;
-// }
-
-// function print(element) {
-//     console.log(element)
-// }
-
-// // numbersAdd.forEach(addFive)
-// // numbersAdd.forEach(print)
-
-// let addedFive = numbersAdd.map(addFive)
-
-// console.log(numbersAdd, addedFive)
-
-//Reduce
-
-// let names = ["Abhijeet", "Kiran"]
-
-// function fl(addedName, name) {
-//     return name + String(addedName).charAt(0) + String(addedName).charAt(addedName.length - 1)
-// }
-
-// names[0].l
-
-// console.log(names.reduce(fl))
-
-//Array filter
-
-// let num = [1,2,3,4,6,7,8]
-
-// num.filter(element => element % 2 == 0).forEach(element => console.log(element))
-
-//Map in js
-
-// const sampleMap = new Map([])
-
-// let someObject = {
-//     model : "MyMOdel",
-//     date : "2023-10-10",
-
-//     printInfo : function() {
-//         console.log(`THis is some object object and the model is ${this.model} and the date is ${this.date}`)
-//     }
-// }
-
-// someObject.printInfo()
-
-//Classes
-
-class Player {
-    score = 0
-
-    constructor(score) {
-        this.score = score
-    }
-    
-    pause() {
-        console.log("You paused the game")
-    }
-
-    exit() {
-        console.log("You exited the game")
-    }
 }
 
-let p = new Player()
-console.log(p.pause())
+// loadFile().then(() => console.log("something is fishy"))
+//     .catch(() => console.log(
+//         "something is not fishy"
+//     ))
+
+//await makes code wait for a promise
+
+async function startAwait() {
+    let someVal = await loadFile()
+    console.log(someVal)
+}
+
+startAwait()
